605215dfcf3de9de659d25f595655e089191dd55
Kyrsten <ksurbayan@gmail.com>
2021-08-27 21:22:26 -0800

    generator function prototype

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,14 @@
 // Assignment code here
+// Declaring variables in global scope
+var passLength;
+var lowerCase;
+var upperCase;
+var numeric;
+var special;
+
 // Function to ask for length of password
 function askLength() {
-  var passLength = parseInt(prompt("How many characters should the password be? It must be at least 8 or up to 128 characters."));
+  passLength = parseInt(prompt("How many characters should the password be? It must be at least 8 or up to 128 characters."));
 
   // Make sure length is between 8 and 128, otherwise, prompt again
   if(passLength < 8 || passLength > 128 || !passLength) {
@@ -14,19 +21,19 @@ function askLength() {
 // Function to ask for different character types
 function askCharType() {
   // Ask to include lowercase
-  var lowerCase = confirm("Would you like to include lowercase characters?");
+  lowerCase = confirm("Would you like to include lowercase characters?");
 
   // Ask to include uppercase
-  var upperCase = confirm("Would you like to include uppercase characters?");
+  upperCase = confirm("Would you like to include uppercase characters?");
 
   // Ask to include numbers
-  var numeric = confirm("Would you like to include numbers?");
+  numeric = confirm("Would you like to include numbers?");
 
   // Ask to include special characters
-  var specialChar = confirm("Would you like to include special characters?");
+  special = confirm("Would you like to include special characters?");
   
   // Make sure at least one char type has been chosen, otherwise, ask again
-  if(!lowerCase && !upperCase && !numeric && !specialChar) {
+  if(!lowerCase && !upperCase && !numeric && !special) {
     alert("Please choose at least one character type!");
     askCharType();
   }
@@ -39,8 +46,36 @@ function generatePassword() {
   // Ask for character types to include
   askCharType();
 
-  // Array of characters
-  
+  // Arrays of character types
+  var lowerCaseChar = "abcdefghijklmnopqrstuvwxyz";
+
+  var upperCaseChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+  var numericChar = "0123456789";
+
+  var specialChar = "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
+
+  // Array for password generation
+  var returnPass = "";
+
+  if(lowerCase) {
+    returnPass += lowerCaseChar;
+  }
+  if(upperCase) {
+    returnPass += upperCaseChar;
+  }
+  if(numeric) {
+    returnPass += numericChar;
+  }
+  if(special) {
+    returnPass += specialChar;
+  }
+
+  for(i = 0; i < passLength; i++) {
+    debugger;
+    returnPass[i] = returnPass[Math.floor(Math.random() * returnPass.length)];
+  }
+  return returnPass;
 }
 
 // Get references to the #generate element
